cc3d353eb84ce868785697858beb8827d4bbb627
Harun Delic <hd125@uowmail.edu.au>
2020-03-24 12:56:26 +1100

    Added a 'APIBuilder' and 'APIRunner' to make calls to the API cleaner to read and modify.

--- public/js/addon.js
+++ public/js/addon.js
@@ -1,1 +1,32 @@
 /* add-on script */
+
+var APIBuilder = function(){
+    this.parameters = {};
+    this.url = "";
+    this.builturl = null;
+    
+    this.build = function(){
+        let uri = [];
+
+        for (let [key, value] of Object.entries(this.parameters)) {
+            uri.push(`${key}=${value}`);
+        }
+
+        uri = encodeURIComponent(uri.join("&"));
+        uri = `${this.url}?${uri}`;
+        this.builturl = uri;
+    };
+};
+
+var APIRunner = function(apibuilder){
+    this.apibuilder = apibuilder;
+    this.success = undefined;
+    this.failure = undefined;
+    this.run = function(){
+        this.apibuilder.build();
+        AP.request(apibuilder.builturl, {
+            error: this.failure,
+            success: this.success
+        });
+    };
+};
