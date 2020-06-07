44f194109a229a48e3e2eea0d9883bcb8a565b88
Crazedseal <Dacrazed@gmail.com>
2020-06-07 18:09:05 +1000

    ui examples

--- client/src/pages/IssueGlancePanel.js
+++ client/src/pages/IssueGlancePanel.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import Button, { ButtonGroup } from '@atlaskit/button';
 import { useLocation } from 'react-router-dom'
 import { userParams } from 'react-router-dom'
+import Lozenge, { ThemeAppearance } from '@atlaskit/lozenge';
+import Select from '@atlaskit/select';
+
+const GROUP_OPTIONS = [
+  {
+    label: 'Machine Learning',
+    options: [
+      { label: 'Use RiskEvader Evaluation', value: 'risk-evader-eval' }
+    ],
+  },
+  {
+    label: 'Manual Overrides',
+    options: [
+	  { label: 'Override: High Risk', value: 'override-high' },
+	  { label: 'Override: Medium Risk', value: 'override-medium' },
+	  { label: 'Override: Low Risk', value: 'override-low' },
+	  { label: 'Don\'t Evaluate', value: 'override-no-eval' },
+    ],
+  },
+];
+
 
 
 
@@ -14,6 +35,12 @@ export default class IssueGlancePanel extends Component {
     return (
 		<div>
 			<h1> HELLO GLANCE PANEL KUN </h1>
+			<Lozenge appearance="removed" isBold>High Risk</Lozenge>
+
+			<Select options={GROUP_OPTIONS} 
+			placeholder="Choose a City" 
+			defaultValue={{ label: 'Use RiskEvader Evaluation', value: 'risk-evader-eval' }}
+			/>
 		</div>
     );
   }
