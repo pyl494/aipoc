1c12154924823e61438d85d79a8b62d98516ccb3
H D <hd125@uowmail.edu.au>
2020-09-02 06:17:34 +1000

    - minor bugfix in the way date is sent to backend.

--- util.js
+++ util.js
@@ -5,7 +5,7 @@ export async function get_issue_and_linked(app, addon, req, res, issueKey) {
 
 	console.log("/rest/api/3/search?jql=" + encodeURI(`issueKey = ${issueKey} OR issue in linkedIssues(${issueKey})`) + "&maxResults=999999&fields=*all&expand=names");
 
-	await new Promise((resolve, reject) => { 
+	await new Promise((resolve, reject) => {
 		httpClient.get({
 			"headers": {
 				"Content-Type": "application/json",
@@ -14,7 +14,7 @@ export async function get_issue_and_linked(app, addon, req, res, issueKey) {
 			"url": "/rest/api/3/search?jql=" + encodeURI(`issueKey=${issueKey} OR issue in linkedIssues(${issueKey})`) + "&maxResults=999999&fields=*all&expand=names"
 		},
 		function(err, response, body) {
-			if (err) { 
+			if (err) {
 				console.log(response.statusCode + ": " + err);
 				res.send("Error: " + response.statusCode + ": " + err);
 				resolve();
@@ -24,10 +24,12 @@ export async function get_issue_and_linked(app, addon, req, res, issueKey) {
 				resolve();
 			}
 		});
+	}).catch((error) => {
+		console.error(err);
 	});
 
 	return issue_and_linked;
-	
+
 }
 
 
@@ -35,7 +37,7 @@ export async function get_all_issues_project(app, addon, req, res, project_key) 
 
 	var issues = [];
 	var httpClient = addon.httpClient(req);
-	await new Promise((resolve, reject) => { 
+	await new Promise((resolve, reject) => {
 		httpClient.get({
 			"headers": {
 				"Content-Type": "application/json",
@@ -44,7 +46,7 @@ export async function get_all_issues_project(app, addon, req, res, project_key) 
 			"url": "/rest/api/3/search?jql=project" + encodeURI(" = " + project_key) + "&maxResults=999999&fields=*all&expand=names"
 		},
 		function(err, response, body) {
-			if (err) { 
+			if (err) {
 				console.log(response.statusCode + ": " + err);
 				res.send("Error: " + response.statusCode + ": " + err);
 				resolve();
@@ -56,6 +58,8 @@ export async function get_all_issues_project(app, addon, req, res, project_key) 
 
 			}
 		});
+	}).catch((error) => {
+		console.error(error);
 	});
 	return issues;
 
